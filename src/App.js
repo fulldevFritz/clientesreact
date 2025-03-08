333e2479fe0e9ab4c1c8ec5f412922cfaf3665c1
Felipe Fritz Pessoa <fritzlumen@hotmail.com>
2025-03-08 12:55:02 -0300

    Concluindo o modal de edição de cliente

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
 
   const [modalIncluir, setModalIncluir] = useState(false);
 
+  const [modalEditar, setModalEditar] = useState(false);
+
   const [clienteSelecionado, setClienteSelecionado] = useState({
     id: '',
     nome: '',
@@ -21,8 +23,17 @@ function App() {
     idade: ''
   });
 
+  const selecionarCliente = (cliente, caso) => {
+    setClienteSelecionado(cliente);
+    (caso === "Editar") && abrirFecharModalEditar()
+  }
+
   const abrirFecharModalIncluir = () => {
     setModalIncluir(!modalIncluir);
+  }
+
+  const abrirFecharModalEditar = () => {
+    setModalEditar(!modalEditar);
   }
 
   const handleChange = e => {
@@ -55,9 +66,30 @@ function App() {
       })
   }
 
+  const pedidoPut = async () => {
+    clienteSelecionado.idade = parseInt(clienteSelecionado.idade);
+    await axios.put(baseUrl+"/"+clienteSelecionado.id, clienteSelecionado)
+      .then(response => {
+        var resposta = response.data;
+        var dadosAuxiliar = data;
+        dadosAuxiliar.map(cliente => {
+          if(cliente.id === resposta.id) {
+            cliente.nome = resposta.nome;
+            cliente.email = resposta.email;
+            cliente.idade = resposta.idade;
+          }
+          return dadosAuxiliar;
+        });
+        abrirFecharModalEditar();
+      }).catch(error => {
+        console.log(error);
+      })
+    }
+
   useEffect(() => {
     pedidoGet();
   })
+  
 
   return (
     <div className="cliente-container">
@@ -85,8 +117,8 @@ function App() {
               <td>{cliente.email}</td>
               <td>{cliente.idade}</td>
               <td>
-                <button className="btn btn-primary">Editar</button> {" "}
-                <button className="btn btn-danger">Excluir</button>
+                <button className="btn btn-primary" onClick={()=> selecionarCliente(cliente, "Editar")} >Editar</button> {" "}
+                <button className="btn btn-danger" onClick={()=> selecionarCliente(cliente, "Excluir")}>Excluir</button>
               </td>
             </tr>
         ))}
@@ -116,8 +148,42 @@ function App() {
           </div>
         </ModalBody>
         <ModalFooter>
-          <button className="btn btn-primary" onClick={()=>pedidoPost()} >Incluir</button> {" "}
+          <button className="btn btn-primary" onClick={()=>pedidoPost()} >Salvar</button> {" "}
           <button className="btn btn-danger" onClick={()=>abrirFecharModalIncluir()}>Cancelar</button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar Cliente</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Id: </label><br /> 
+            <input type="text" className="form-control" 
+            readOnly value={clienteSelecionado && clienteSelecionado.id}/><br />
+            <label>Nome: </label>
+            <br />
+            <input type="text" className="form-control" name="nome" onChange={handleChange}
+            value={clienteSelecionado && clienteSelecionado.nome} />
+            <br />
+            <label>Email: </label>
+            <br />
+            <input type="text" className="form-control" name="email" onChange={handleChange}
+            value={clienteSelecionado && clienteSelecionado.email} />
+            <br />
+            <label>Idade: </label>
+            <br />
+            <input type="text" className="form-control" name="idade" onChange={handleChange} 
+            value={clienteSelecionado && clienteSelecionado.idade} />
+            <br />
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={()=>pedidoPut()} >Salvar</button> {" "}
+          <button className="btn btn-danger" onClick={()=>abrirFecharModalEditar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
     </div>
