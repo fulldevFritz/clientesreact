2f1deea56a034ea59d3348660d0b84850393edf2
Felipe Fritz Pessoa <fritzlumen@hotmail.com>
2025-03-08 10:16:40 -0300

    Get dos clientes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'bootstrap';
+import logoCadastro from './assets/cadastro.png';
 
 function App() {
+
+  const baseUrl = "https://localhost:5001/api/Clientes";
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async () => {
+    await axios.get(baseUrl)
+      .then(response => {
+        setData(response.data);
+      }).catch(error => {
+        console.log(error);
+      })
+  }
+
+  useEffect(() => {
+    pedidoGet();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Fritz!</h1>
+      <br />
+      <h3>Cadastro de Clientes</h3>
+      <header>
+        <img src={logoCadastro} alt='Cadastro' />
+        <button className="btn btn-success">Incluir Novo Cliente</button>
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          { data.map(cliente => (
+            <tr key={cliente.id}>
+              <td>{cliente.id}</td>
+              <td>{cliente.nome}</td>
+              <td>{cliente.email}</td>
+              <td>{cliente.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
